a77b9dcc875412a82c41b6e254bc2f6b699ee089
Jeremy Jones <jeremyj@letifer.org>
2010-07-02 14:39:26 -0500

    setup and register with pipy

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='ScribeHandler',
+    author='Jeremy Jones',
+    author_email='jeremyj@letifer.org',
+    url='http://github.com/jmj/ScribeHandler',
+    version='0.01',
+    py_modules=['ScribeHandler'],
+    license='GPLv2',
+    long_description=open('README').read()
+    )
